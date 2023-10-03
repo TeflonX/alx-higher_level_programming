19b37a8bc22c223def6e90d24824a92d703fd5f2
TeflonX <wisdom.ikegwuru@yahoo.com>
2023-10-03 22:42:26 +0000

    task 15: used and condition

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-    if n >= 0 or n < len(str):
+    if n >= 0 and n < len(str):
         new_str = str[:n] + str[n+1:]
         return new_str
     else:
