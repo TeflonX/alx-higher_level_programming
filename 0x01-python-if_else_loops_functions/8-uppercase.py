721286a6b1bd36120803c490f35a293e2ccc1b65
TeflonX <wisdom.ikegwuru@yahoo.com>
2023-10-03 21:15:19 +0000

    task 8 and 9: used if statement and loop

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def uppercase(str):
     for i in str:
-        if ord(i) >= 97 and ord(i) <= 122:
-            print("{}".format(chr(ord(i) - 32)), end='')
-        else:
-            print(i, end='')
+        char = ord(i)
+        if char >= 97 and char <= 122:
+            char -= 32
+        print("{:c}".format(char), end='')
     print()
--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -2,6 +2,9 @@
 def print_last_digit(number):
     if number >= 0:
         print(number % 10, end='')
+        return number % 10
     else:
-        number = number * -1
-        print(-1 * (number % 10), end='')
+        number *= -1
+        number = number % 10
+        print(number, end='')
+        return (number % 10)
