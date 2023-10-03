bece226312116a072032139374f9869605892a83
TeflonX <wisdom.ikegwuru@yahoo.com>
2023-10-03 22:32:28 +0000

    task 16: corrected return value

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -3,6 +3,6 @@ def magic_calculation(a, b, c):
     if a < b:
         return C
     elif c > b:
-        return (a * b) - c
-    else:
         return (a + b)
+    else:
+        return (a * b) - c
