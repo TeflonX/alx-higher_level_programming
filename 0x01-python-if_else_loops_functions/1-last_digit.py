ff31289360017673ed446bfddaddb7de6403568d
TeflonX <wisdom.ikegwuru@yahoo.com>
2023-10-04 01:15:38 +0000

    task 4: used hex function

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-if number < 0:
-    positive_num = number * -1
-    print("Last digit of", number, "is", -1 * (positive_num % 10))
-else:
-    print("Last digit of", number, "is", number % 10)
+if (number % 10) > 5:
+    print("Last digit of {} is {} and is greater than 5".format(number,
+        number % 10)
+elif (number % 10) == 0:
+    print("Last digit of {} is {} and is 0".format(number,
+            number % 10)
+elif (number % 10) > 0 and (number % 10) < 6:
+    print("Last digit of {} is {} and is less than 6 and not 0".format(number,
+            number % 10)
