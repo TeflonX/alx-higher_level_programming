dcb82dd871e347bec3637ca2d63f88eabfca2aa7
TeflonX <wisdom.ikegwuru@yahoo.com>
2023-10-05 23:05:40 +0000

    task 4: corrected the range function

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,6 +2,6 @@
 if __name__ == '__main__':
     import hidden_4
     new_list = dir(hidden_4)
-    for i in range(new_list[i]):
+    for i in range(len(new_list)):
         if "__" not in new_list[i]:
             print(new_list[i])
