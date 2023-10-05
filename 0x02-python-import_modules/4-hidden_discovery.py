156456bbf6d09b6e8dc8fa63e7145ca97b946490
TeflonX <wisdom.ikegwuru@yahoo.com>
2023-10-05 22:59:13 +0000

    task 4:used dir

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,5 +1,7 @@
 #!/usrbin/python3
-new_list = dir()
-for i in new_list:
-    if "__" not in i:
-
+if __name__ == '__main__':
+    import hidden_4
+    new_list = dir(hidden_4)
+    for i in range(new_list[i]):
+        if "__" not in new_list[i]:
+            print(new_list[i])
