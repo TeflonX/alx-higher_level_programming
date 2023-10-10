eccba5ba0c74d4010e5fce9a88a01008c71c8a92
TeflonX <wisdom.ikegwuru@yahoo.com>
2023-10-10 04:17:52 +0000

    task 8: removed else statement

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,7 +2,6 @@
 def multiple_returns(sentence):
     if not sentence:
         new_tuple = (0, None)
-    else:
-        new_tuple = ()
-        new_tuple = (len(sentence), sentence[0],)
-        return (new_tuple)
+    new_tuple = ()
+    new_tuple = (len(sentence), sentence[0],)
+    return (new_tuple)
