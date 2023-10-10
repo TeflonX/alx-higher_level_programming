84a5c675be0bc02b005f43ed044f93147f3bd6c8
TeflonX <wisdom.ikegwuru@yahoo.com>
2023-10-10 01:03:07 +0000

    task 0 and 11: used str format and ensured linter compliance

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -5,5 +5,5 @@ def delete_at(my_list=[], idx=0):
     else:
         for i in range(len(my_list)):
             if i == idx:
-                del(my_list[i])
+                del (my_list[i])
         return (my_list)
