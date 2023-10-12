f7eef91d6edc38d435b058ed19999c56228df22e
TeflonX <wisdom.ikegwuru@yahoo.com>
2023-10-12 13:44:17 +0000

    task 10: catered for a Noge arguent and an empty list

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    key_list = list(a_dictionary)
-    if not a_dictionary:
+    if a_dictionary is None:
         return (None)
-    if a_dictionary is not None:
-        new_list = []
-        for x in key_list:
-            new_list.append(a_dictionary[x])
-        new_list.sort()
-        return (new_list[-1])
+
+    key_list = list(a_dictionary)
+    if not key_list:
+        return (None)
+
+    new_list = []
+    for x in key_list:
+        new_list.append(a_dictionary[x])
+    new_list.sort()
+    return (new_list[-1])
