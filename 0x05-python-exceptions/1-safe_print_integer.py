b7d96e46c1aa214b1d1e463844cad0bd49410353
Teflonx <wisdom.ikegwuru@yahoo.com>
2023-10-24 01:40:21 +0000

    task 2: used exception instead

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,6 +3,6 @@ def safe_print_integer(value):
     ret_value = True
     try:
         print("{:d}".format(value))
-    except TypeError:
+    except Exception:
         ret_value = False
     return (ret_value)
