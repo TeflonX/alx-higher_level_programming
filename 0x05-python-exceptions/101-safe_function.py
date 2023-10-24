7307c5909d53bb73571324500c8d88538cfb13b8
Teflonx <wisdom.ikegwuru@yahoo.com>
2023-10-24 01:12:09 +0000

    task 7 and 8: solved two advanced tasks

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import sys
+def safe_function(fct, *args):
+    try:
+        result = fct(*args)
+    except Exception as error:
+        result = None
+        print("Exception: {}".format(error), file=sys.stderr)
+    return (result)
