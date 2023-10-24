8611d0dcc8122d092b91e4006ba75554effa2700
Teflonx <wisdom.ikegwuru@yahoo.com>
2023-10-24 01:42:29 +0000

    task 0: fixed PEP8 error

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,7 +6,7 @@ def safe_print_list(my_list=[], x=0):
             print(my_list[i], end='')
             element += 1
         print()
-    except:
+    except Exception:
         pass
 
     return (element)
