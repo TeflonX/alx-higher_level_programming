e6e6fb124ad486fda35b21b72d5a4b0c9972f292
Teflonx <wisdom.ikegwuru@yahoo.com>
2023-10-25 02:59:29 +0000

    task 3: calculates the area of the square

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Defines a class named Square"""
+
+
+class Square:
+    """calculates the area of a square and prints it"""
+    def __init__(self, size=0):
+        """initializes the size of the square
+
+        Args:
+            size(int): size of the square
+
+        Raises:
+            TypeError: when size is not an integer
+            ValueError: when size is less than 0
+        """
+        self.__size = size
+        if not type(size) == int:
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+
+    def area(self):
+        """Calculates the area of the square
+
+        Returns:
+            area: area of the square
+        """
+        area = (self.__size) * (self.__size)
+        return (area)
