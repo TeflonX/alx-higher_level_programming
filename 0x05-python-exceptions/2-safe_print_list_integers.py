c63b76aaf98b5cb1b66a8e151a1b1464a32a591e
Teflonx <wisdom.ikegwuru@yahoo.com>
2023-10-26 12:50:39 +0000

    task 2: solved the error

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -7,6 +7,8 @@ def safe_print_list_integers(my_list=[], x=0):
             elements += 1
         except IndexError:
             raise
+        except Exception:
+            pass
     print()
 
     return (elements)
