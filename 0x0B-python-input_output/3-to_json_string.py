50a389b986556abdb2bfe75f6874dbd0f3373aab
Teflonx <wisdom.ikegwuru@yahoo.com>
2023-11-08 03:09:22 +0000

    task 3: returned a JSON object

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+a function that returns the JSON representation of an object (string)
+"""
+
+
+import json
+def to_json_string(my_obj):
+    """
+    Returns json.dump(my_obj)
+    """
+    return json.dumps(my_obj)
