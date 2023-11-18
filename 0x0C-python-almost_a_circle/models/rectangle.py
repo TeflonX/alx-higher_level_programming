95e5da7458c32fd0c608b3fbe6fe74b563c60896
Teflonx <wisdom.ikegwuru@yahoo.com>
2023-11-18 22:45:26 +0000

    task 8: finally got it

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -136,6 +136,22 @@ class Rectangle(Base):
         for i in range(self.__height):
             print(' ' * self.__x + '#' * self.__width)
 
+    def update(self, *args):
+        """
+        A public method that assigns an argument to each attribute
+        """
+        if len(args) >= 1:
+            self.id = args[0]
+        if len(args) >= 2:
+            self.width = args[1]
+        if len(args) >= 3:
+            self.height = args[2]
+        if len(args) >= 4:
+            self.x = args[3]
+        if len(args) >= 5:
+            self.y = args[4]
+
+
     def __str__(self):
         """
         return string representation of the rectangle
