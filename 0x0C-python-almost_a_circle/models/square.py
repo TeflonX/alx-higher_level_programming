6ddb4be814edcb02ff850d6c1dd52cd6660b1038
TeflonX <wisdom.ikegwuru@yahoo.com>
2023-11-29 05:54:56 +0000

    task 10: done

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/bash
+"""
+A Square class that inherits from the rectangle class
+"""
+from rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """
+    A square class
+    """
+    def __init__(self, size, x=0, y=0, id=None):
+        """
+        initializes the attributes of the class with the base class
+        attribute
+
+        Attr:
+            size: width or height of rectangle
+            x: int
+            y: int
+            id: int
+        """
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """
+        Returns the string representation of a square
+        """
+        return f"[Square] (<self.id>) <self.x>/<self.y> - <self.size>"
