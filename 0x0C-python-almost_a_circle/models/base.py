612b576befc14d08b8db3302ffc46a74812aa974
TeflonX <wisdom.ikegwuru@yahoo.com>
2023-11-30 05:20:12 +0000

    task 17: used json.loads

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -24,12 +24,22 @@ class Base:
     def to_json_string(list_dictionaries):
         import json
         """
-        that returns the JSON string representation of list_dictionaries
+        A static method that returns the JSON string representation of
+        list_dictionaries
         """
         if list_dictionaries is None:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
+
+    @staticmethod
+    def from_json_string(json_string):
+        import json
+        """
+        A static method that returns the list of the JSON string
+        representation json_string
+        """
+        return json.loads(json_string)
 
     @classmethod
     def save_to_file(cls, list_objs):
