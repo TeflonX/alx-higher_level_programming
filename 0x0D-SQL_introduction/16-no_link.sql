0b43224e94f665d12e13c296e0cf5f0a72b6e040
TeflonX <wisdom.ikegwuru@yahoo.com>
2023-12-14 21:02:56 +0000

    done and dusted

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,6 +1,3 @@
 -- a script that lists all records of the table second_table of the database
 -- hbtn_0c_0 in your MySQL server.
-SELECT count, name
-FROM second_table
-WHERE name IS NOT NULL AND name != ''
-ORDER BY score DESC;
+SELECT count, name FROM second_table WHERE name IS NOT NULL AND name != '' ORDER BY score DESC;
